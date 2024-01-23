24672e48f60431e6292188ba08b3581bea6265d9
RParker505 <rchiado75@gmail.com>
2024-01-22 17:21:23 -0700

    Require morgan, fs, path

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 const express = require('express'); //import express to be used locally
+    morgan = require('morgan');
+    fs = require('fs');
+    path = require('path');
+    
 const app = express(); //use this variable to route HTTP requests and responses
