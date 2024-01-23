a7b74cc7bb6824e51e613fdf0c961aab15934b43
RParker505 <rchiado75@gmail.com>
2024-01-23 05:12:32 -0700

    Add topMovies list and app.get for /movies path

--- index.js
+++ index.js
@@ -4,3 +4,52 @@ const express = require('express'); //import express to be used locally
     path = require('path');
     
 const app = express(); //use this variable to route HTTP requests and responses
+
+let topMovies = [
+    {
+      title: 'Midsommar',
+      director: 'Ari Aster'
+    },
+    {
+      title: 'IT',
+      director: 'Andy Muschietti'
+    },
+    {
+      title: 'The Exorcist',
+      director: 'William Friedkin'
+    },
+    {
+      title: 'Talk to Me',
+      director: ['Danny Philippou','Michael Philippou']
+    },
+    {
+      title: 'Barbarian',
+      director: 'Zach Cregger'
+    },
+    {
+      title: 'Insidious',
+      director: 'James Wan'
+    },
+    {
+      title: 'Hereditary',
+      director: 'Ari Aster'
+    },
+    {
+      title: 'Silence of the Lambs',
+      director: 'Jonathan Demme'
+    },
+    {
+      title: 'The Mist',
+      director: 'Frank Darabont'
+    },
+    {
+      title: 'Get Out',
+      director: 'Jordan Peele'
+    }
+  ];
+  
+  // return list of top movies when request path is /movies
+  app.get('/movies', (req, res) => {
+    res.json(topMovies);
+  });
+  
