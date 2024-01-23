67df370fb37a62a4d0b265bd1aaf7baca3a7c38f
RParker505 <rchiado75@gmail.com>
2024-01-23 05:26:58 -0700

    Add error-handling middleware

--- index.js
+++ index.js
@@ -69,6 +69,12 @@ app.get('/', (req, res) => {
 app.get('/movies', (req, res) => {
     res.json(topMovies);
   });
+
+  // error-handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+  });
   
   // listen for requests
 app.listen(8080, () => {
