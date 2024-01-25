cdadbdb62abee970c94c38cdad539e7458d4e918
RParker505 <rchiado75@gmail.com>
2024-01-24 20:19:06 -0700

    Capitalize keys in the movies array

--- index.js
+++ index.js
@@ -10,34 +10,34 @@ let users = []
 
 let movies = [
     {
-        'title': 'Midsommar',
-        'description': 'A couple travels to Northern Europe to visit a rural hometown\'s fabled Swedish mid-summer festival. What begins as an idyllic retreat quickly devolves into an increasingly violent and bizarre competition at the hands of a pagan cult.',
-        'director': {
-            'name': 'Ari Aster',
-            'bio': 'Ari Aster is an American filmmaker. He became best known for writing and directing Hereditary, Midsommar, and Beau Is Afraid, all of which were released by A24.',
-            'birth_year': 1986
+        'Title': 'Midsommar',
+        'Description': 'A couple travels to Northern Europe to visit a rural hometown\'s fabled Swedish mid-summer festival. What begins as an idyllic retreat quickly devolves into an increasingly violent and bizarre competition at the hands of a pagan cult.',
+        'Director': {
+            'Name': 'Ari Aster',
+            'Bio': 'Ari Aster is an American filmmaker. He became best known for writing and directing Hereditary, Midsommar, and Beau Is Afraid, all of which were released by A24.',
+            'Birth_year': 1986
         },
-        'genre': {
-            'name': 'Folk horror',
-            'description': 'Folk horror is a subgenre of horror film and horror fiction that uses elements of folklore to invoke fear and foreboding.'
+        'Genre': {
+            'Name': 'Folk horror',
+            'Description': 'Folk horror is a subgenre of horror film and horror fiction that uses elements of folklore to invoke fear and foreboding.'
         },
-        'image': 'https://m.media-amazon.com/images/M/MV5BMzQxNzQzOTQwM15BMl5BanBnXkFtZTgwMDQ2NTcwODM@._V1_.jpg',
-        'featured': false
+        'Image': 'https://m.media-amazon.com/images/M/MV5BMzQxNzQzOTQwM15BMl5BanBnXkFtZTgwMDQ2NTcwODM@._V1_.jpg',
+        'Featured': false
     },
     {
-        'title': 'IT',
-        'description': 'In the summer of 1989, a group of bullied kids band together to destroy a shape-shifting monster, which disguises itself as a clown and preys on the children of Derry, their small Maine town.',
-        'director': {
-            'name': 'Andy Muschietti',
-            'bio': 'Andy Muschietti was born on August 26, 1973, in Buenos Aires, Federal District, Argentina. He is a producer and director, known for Mama (2013), It (2017) and It Chapter Two (2019).',
-            'birth_year': 1973
+        'Title': 'IT',
+        'Description': 'In the summer of 1989, a group of bullied kids band together to destroy a shape-shifting monster, which disguises itself as a clown and preys on the children of Derry, their small Maine town.',
+        'Director': {
+            'Name': 'Andy Muschietti',
+            'Bio': 'Andy Muschietti was born on August 26, 1973, in Buenos Aires, Federal District, Argentina. He is a producer and director, known for Mama (2013), It (2017) and It Chapter Two (2019).',
+            'Birth_year': 1973
         },
-        'genre': {
-            'name': 'Supernatural horror',
-            'description': 'Supernatural horror film is a film genre that combines aspects of supernatural film and horror film. Supernatural occurrences in such films often include ghosts and demons, and many supernatural horror films have elements of religion.'
+        'Genre': {
+            'Name': 'Supernatural horror',
+            'Description': 'Supernatural horror film is a film genre that combines aspects of supernatural film and horror film. Supernatural occurrences in such films often include ghosts and demons, and many supernatural horror films have elements of religion.'
         },
-        'image': 'https://upload.wikimedia.org/wikipedia/en/5/5a/It_%282017%29_poster.jpg',
-        'featured': false
+        'Image': 'https://upload.wikimedia.org/wikipedia/en/5/5a/It_%282017%29_poster.jpg',
+        'Featured': false
     }
 ]
 
