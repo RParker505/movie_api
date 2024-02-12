92961fefe227058b3d5132129033ab37f61d8de8
RParker505 <rchiado75@gmail.com>
2024-02-12 06:08:59 -0700

    Apply JWT authentication to /movies endpoint

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ app.delete('/users/:Username', async (req, res) => {
 });
 
 // READ/GET to return the full list of movies
-app.get('/movies', async (req, res) => {
+app.get('/movies', passport.authenticate('jwt', {session: false}), async (req, res) => {
   await Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
