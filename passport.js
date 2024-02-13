3f197630242296de401a57b94d819431b89c9368
RParker505 <rchiado75@gmail.com>
2024-02-13 06:18:52 -0700

    Add comments for clarification

--- passport.js
+++ passport.js
@@ -5,7 +5,9 @@ const passport = require('passport'),
 
 let Users = Models.User,
     JWTStrategy = passportJWT.Strategy,
+    //passport-jwt.Strategy implements a JWT authentication strategy for Passport. It requires a secret key to verify the JWT and checks for the JWT in the header
     ExtractJWT = passportJWT.ExtractJwt;
+    //passport-jwt.ExtractJwt is a utility that extracts the JWT from incoming requests. Specifies where and how Passport should look for the JWT.
 
 //Takes username and password from request body and use Mongoose findOne to determine if user exists
 passport.use(
@@ -41,7 +43,7 @@ passport.use(
 passport.use(new JWTStrategy ({
     jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),//JWT is extracted from HTTP request header
     secretOrKey: 'your_jwt_secret'//use secret key to verify signature of the JWT (ensure client is who it says it is and JWT hasn't been altered)
-}, async (jwtPayload, callback) => {
+}, async (jwtPayload, callback) => {//take the object literal of the decoded JWT payload as a parameter
     return await Users.findById(jwtPayload._id)
         .then((user) => {
             return callback(null, user);
