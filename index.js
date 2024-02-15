042ef1fd817947dc12dc448e4afd6a794a091d02
RParker505 <rchiado75@gmail.com>
2024-02-15 05:28:22 -0700

    Add input validation for endpoint to create new user

--- index.js
+++ index.js
@@ -35,7 +35,13 @@ app.use(express.static('public'));
   Email: String,
   Birthday: Date
 }*/
-app.post('/users', async (req, res) => {
+app.post('/users', [
+  //input validation here
+  check('Username', 'Username is required').notEmpty(),
+  check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+  check('Password', 'Password is required').notEmpty(),
+  check('Email', 'Email does not appear to be valid').isEmail()
+], async (req, res) => {
   let hashedPassword = Users.hashPassword(req.body.Password);
   await Users.findOne({Username: req.body.Username})//Query Users model to check if username from client already exists. If so, alert them.
   .then((user) => {
