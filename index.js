52610e86f4b79df14bb948e89ae389256f9a7a2c
RParker505 <rchiado75@gmail.com>
2024-04-07 09:17:02 -0600

    Update CORS to allow access from Netlify app

--- index.js
+++ index.js
@@ -17,7 +17,18 @@ const Users = Models.User;
 mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const cors = require('cors');
-app.use(cors());
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://spookyvibes.netlify.app/'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 let auth = require('./auth.js')(app); //(app) ensures Express is available in the .auth.js file
 const passport = require('passport');
