a938289cb4ce793b6ae321f377197e5c3ac883c2
RParker505 <rchiado75@gmail.com>
2024-04-08 05:34:15 -0600

    Revert "Update CORS to allow access from Netlify app"
    This reverts commit 52610e86f4b79df14bb948e89ae389256f9a7a2c.

--- index.js
+++ index.js
@@ -17,18 +17,7 @@ const Users = Models.User;
 mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://spookyvibes.netlify.app/'];
-
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
 
 let auth = require('./auth.js')(app); //(app) ensures Express is available in the .auth.js file
 const passport = require('passport');
